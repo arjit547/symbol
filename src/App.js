b0a5709f7ec32b9f1cd50b35cd878936d675af70
arjit547 <97424724+arjit547@users.noreply.github.com>
2022-12-15 11:12:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
   
 function App() {
     return ( 
-      <center><h1>  MoogleLabs Mohali  </h1></center>
+      <center><h1>  MoogleLabs Group of Company  </h1></center>
     );
 }
   
