a644a403f1b9d370b820807f99c8d76bfaaa61b6
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-12-29 11:53:18 +0530

    Update run_dependency_check.sh

--- run_dependency_check.sh
+++ run_dependency_check.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
 BUCKET_NAME="owzapsst"
+DC_VERSION="6.2.1"  # Use a version that is available on Bintray
 
 # Download and run OWASP Dependency-Check
-wget https://dl.bintray.com/jeremy-long/owasp/dependency-check-5.2.4-release.zip
-unzip dependency-check-*.zip
-cd dependency-check-*
+wget "https://dl.bintray.com/jeremy-long/owasp/dependency-check-${DC_VERSION}-release.zip" -O dependency-check.zip
+
+if [ $? -ne 0 ]; then
+  echo "Error downloading Dependency-Check ZIP."
+  exit 1
+fi
+
+unzip dependency-check.zip
+
+if [ $? -ne 0 ]; then
+  echo "Error unzipping Dependency-Check."
+  exit 1
+fi
+
+cd dependency-check-${DC_VERSION}-release
+
+# Run Dependency-Check scan
 ./bin/dependency-check.sh --scan .
+
+if [ $? -ne 0 ]; then
+  echo "Error running Dependency-Check scan."
+  exit 1
+fi
 
 # Sync Dependency-Check reports to S3 bucket
 aws s3 sync .dependency-check/ s3://${BUCKET_NAME}/dependency-check
+
+if [ $? -ne 0 ]; then
+  echo "Error syncing Dependency-Check reports to S3."
+  exit 1
+fi
