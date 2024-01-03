2e7d8c3c263c6eb0ce58420193634c7460028930
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-01-03 10:55:58 +0530

    Update run_checkov.sh

--- run_checkov.sh
+++ run_checkov.sh
@@ -14,11 +14,11 @@ echo "Environment variables after installing Checkov:"
 env
 echo "PATH after installing Checkov: $PATH"
 
-# Run Checkov on your IaC code and generate HTML report
-/usr/local/bin/checkov -d . -o html -f > checkov_output.html
+# Run Checkov on your IaC code and generate text report
+/usr/local/bin/checkov -d . -o txt -f > checkov_output.txt
 
 # Set your S3 bucket name
 BUCKET_NAME="cerebruchecov-artifact"
 
 # Upload the Checkov report to S3
-aws s3 cp checkov_output.html s3://${BUCKET_NAME}/checkov_output.html
+aws s3 cp checkov_output.txt s3://${BUCKET_NAME}/checkov_output.txt
