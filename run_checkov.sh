a3a9fc066a3326ffdcd06f27f86fac856bc1cba5
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-01-03 10:48:02 +0530

    Update run_checkov.sh

--- run_checkov.sh
+++ run_checkov.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Print environment variables and path for debugging
+env
+echo $PATH
+
 # Install Checkov
-curl -L https://github.com/bridgecrewio/checkov/releases/latest/download/checkov_linux_amd64 -o checkov
-chmod +x checkov
-mv checkov /usr/local/bin/checkov
+curl -L https://github.com/bridgecrewio/checkov/releases/latest/download/checkov_linux_amd64 -o /usr/local/bin/checkov
+chmod +x /usr/local/bin/checkov
 
 # Run Checkov on your IaC code and generate HTML report
-checkov -d . -o html -f > checkov_output.html
+/usr/local/bin/checkov -d . -o html -f > checkov_output.html
 
 # Set your S3 bucket name
 BUCKET_NAME="cerebruchecov-artifact"
